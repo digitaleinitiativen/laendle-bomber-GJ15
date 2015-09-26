a3c0c29512d11b079354bc317cccc2f054560e80
Andreas <andreas@wilburger.software>
2015-09-26 14:25:38 +0200

    Revert "t"
    This reverts commit 2daf121787da2817bce07e8d6532b2de36db268d.

--- Client/Player.js
+++ Client/Player.js
@@ -1,8 +1,7 @@
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 var Player = (function (_super) {
     __extends(Player, _super);
@@ -11,4 +10,3 @@ var Player = (function (_super) {
     }
     return Player;
 })(Phaser.Sprite);
-//# sourceMappingURL=Player.js.map
