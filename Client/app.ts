f618192add724d11be2c75038d26e86081197a9e
Andreas <andreas@wilburger.software>
2015-09-26 13:01:29 +0200

    tiles

--- Client/app.ts
+++ Client/app.ts
@@ -5,18 +5,29 @@
     }
 
     game: Phaser.Game;
+    map: Phaser.Tilemap;
+    layerWalls: Phaser.TilemapLayer;
     cursors: Phaser.CursorKeys;
     player: Player;
 
     preload() {
         this.game.load.image('player', 'assets/player.png');
+        this.game.load.tilemap('map', 'assets/tilemap1.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'assets/gridtiles.png');
     }
 
     create() {
 
-        this.game.physics.startSystem(Phaser.Physics.P2JS);
-
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.world.setBounds(0, 0, 800, 800);
+
+        this.map = this.game.add.tilemap('map');
+        this.map.addTilesetImage('tiles');
+
+        this.layerWalls = this.map.createLayer('walls');
+        this.layerWalls.resizeWorld();
+
+        this.map.setCollisionBetween(1, 12);
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
@@ -25,9 +36,15 @@
         this.game.physics.enable(this.player);
         this.player.body.gravity.x = 1;
         this.player.body.gravity.y = 1;
+        this.game.camera.follow(this.player);
+
+        this.player.body.bounce.set(0.6);
+        this.player.body.tilePadding.set(32);
     }
 
     update() {
+        this.game.physics.arcade.collide(this.player, this.layerWalls);
+
         if (this.cursors.left.isDown) {
             this.player.body.velocity.x = -100;
             this.player.body.velocity.y = 0;
