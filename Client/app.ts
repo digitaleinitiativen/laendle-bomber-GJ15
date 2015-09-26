4862828a266ab66565d3888a8b79cf113093eaa7
Philip Heimböck <philipheimboeck@gmail.com>
2015-09-26 13:27:07 +0200

    Added new map

--- Client/app.ts
+++ Client/app.ts
@@ -1,4 +1,7 @@
-﻿class LaendleBomber {
+///<reference path="phaser/phaser.d.ts"/>
+///<reference path="Player.ts"/>
+
+class LaendleBomber {
 
     constructor() {
         this.game = new Phaser.Game(800, 800, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update, render: this.render });
@@ -7,13 +10,15 @@
     game: Phaser.Game;
     map: Phaser.Tilemap;
     layerWalls: Phaser.TilemapLayer;
+    layerBlocks: Phaser.TilemapLayer;
+    layerFloor: Phaser.TilemapLayer;
     cursors: Phaser.CursorKeys;
     player: Player;
 
     preload() {
         this.game.load.image('player', 'assets/player.png');
-        this.game.load.tilemap('map', 'assets/tilemap1.json', null, Phaser.Tilemap.TILED_JSON);
-        this.game.load.image('tiles', 'assets/gridtiles.png');
+        this.game.load.tilemap('map', 'assets/maps/BombMap1.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'assets/maps/BombMap1.png');
     }
 
     create() {
@@ -24,18 +29,25 @@
         this.map = this.game.add.tilemap('map');
         this.map.addTilesetImage('tiles');
 
+        this.layerFloor = this.map.createLayer('floor');
+        this.layerFloor.resizeWorld();
+
         this.layerWalls = this.map.createLayer('walls');
         this.layerWalls.resizeWorld();
 
-        this.map.setCollisionBetween(1, 12);
+        this.layerBlocks = this.map.createLayer('blocks');
+        this.layerBlocks.resizeWorld();
+
+        this.map.setCollisionBetween(0, 10);
+        this.map.setCollisionBetween(0, 34);
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
-        this.player = this.game.add.sprite(0, 0, 'player');
+        this.player = this.game.add.sprite(10, 10, 'player');
 
         this.game.physics.enable(this.player);
-        this.player.body.gravity.x = 1;
-        this.player.body.gravity.y = 1;
+        //this.player.body.gravity.x = 1;
+        //this.player.body.gravity.y = 1;
         this.game.camera.follow(this.player);
 
         this.player.body.bounce.set(0.6);
@@ -44,6 +56,7 @@
 
     update() {
         this.game.physics.arcade.collide(this.player, this.layerWalls);
+        this.game.physics.arcade.collide(this.player, this.layerBlocks);
 
         if (this.cursors.left.isDown) {
             this.player.body.velocity.x = -100;
